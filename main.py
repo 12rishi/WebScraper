7795a36b54cf4bfd1b798e9ff9082382470f8bf7
rishithapa <thapajack123@gmail.com>
2025-01-02 17:57:17 +0545

    learned about navigating using parent and child properties of find method

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import bs4.element
 from bs4 import BeautifulSoup
 import requests
 search=input("enter the search query")
 params={"q":search}
-r=requests.get("https://www.tripadvisor.com/Restaurants-g293890-c31-Kathmandu_Kathmandu_Valley_Bagmati_Zone_Central_Region.html",params=params)
+r=requests.get("http://www.bing.com/search",params=params)
 print(r.status_code)
 soup=BeautifulSoup(r.text,"html.parser")
-print(soup.prettify())
+print(type(soup.prettify()))
+results=soup.find("ol",{"id":"b_results"})
+lists=results.findAll("li")
+for item in lists:
+    item_text=item.find("a").text
+    item_href=item.find("a").attrs["href"]
+    children=item.find("h2")
+    print("next sibling of h2 is",children.next_sibling.find("p",{"class":"b_lineclamp2"}))
